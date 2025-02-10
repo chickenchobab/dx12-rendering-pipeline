474df2ccbac566994699f8c201111ecf34f3b9a4
chickenchobab <hew980812@gmail.com>
2025-02-10 13:47:59 +0900

    Setup camera and light

---
+++ Engine/GameObject.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "Component.h"
+#include "Object.h"
+
+class Transform;
+class MeshRenderer;
+class MonoBehaviour; 
+class Camera;
+class Light;
+
+class GameObject : public Object, public enable_shared_from_this<GameObject>
+{
+public:
+	GameObject();
+	virtual ~GameObject();
+
+	void Awake();
+	void Start();
+	void Update();
+	void LateUpdate();
+	void FinalUpdate();
+
+	shared_ptr<Component> GetFixedComponent(COMPONENT_TYPE type);
+
+	shared_ptr<Transform> GetTransform();
+	shared_ptr<MeshRenderer> GetMeshRenderer();
+	shared_ptr<Camera> GetCamera();
+	shared_ptr<Light> GetLight();
+
+	void AddComponent(shared_ptr<Component> component);
+
+private:
+	array<shared_ptr<Component>, FIXED_COMPONENT_COUNT> _components;
+	vector<shared_ptr<MonoBehaviour>> _scripts;
+};
+
